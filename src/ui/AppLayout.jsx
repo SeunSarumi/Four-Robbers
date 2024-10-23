95c170ac83083893f10407fb73e6785f96133e4f
SeunSarumi <seunsarumi@gmail.com>
2024-10-23 18:24:15 +0100

    AppLayout built

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import "../styles/AppLayout.css";
 
 function AppLayout() {
   return (
-    <div>
-      AppLayout
-      <Outlet />
+    <div className="applayout">
+      <img src="/images/nav-image.png" alt="" />
+      <div className="layout">
+        <Outlet />
+      </div>
     </div>
   );
 }
