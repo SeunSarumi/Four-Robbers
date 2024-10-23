8a252ff60338588c38b608834b2c38a151bbe786
SeunSarumi <seunsarumi@gmail.com>
2024-10-23 18:12:02 +0100

    react router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./index.css";
+import Homepage from "./pages/Homepage";
+import Buynow from "./pages/Buynow";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./ui/Error";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "buynow",
+        element: <Buynow />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
